f10e044d00369d45b36311b665437d96d92ba348
UmamaheswariB <umamaheswari.b89@gmail.com>
2023-09-18 11:56:18 +0530

    html

---
+++ basic.js
@@ -0,0 +1,22 @@
+function addition(x1 ,x2){
+    return x1+x2;
+}
+function subtraction(y1,y2){
+    return y1-y2;
+}
+function modulus(a,b){
+    return a%b;
+}
+function division(c,d){
+    return c/d;
+}
+
+let add1=addition(23,76);
+let add2=addition(30,75);
+let mod=modulus(30,6);
+let div=division(30,4);
+
+console.log(add1)
+console.log(add2);
+console.log(mod);
+console.log(div);
